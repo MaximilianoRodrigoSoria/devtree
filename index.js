a75942aa80027d7ecf7f783199a6d5f57a3cd700
Maximiliano Rodrigo Soria <MaximilianoRodrigoSoria@gmail.com>
2025-10-21 16:55:19 -0300

    set up Express server with basic routing

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello, World!");
+import  express from 'express';
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//Routing
+app.get('/', (req, res) => {
+        res.send('Hello World!');
+})
+
+app.get('/ecommerce', (req, res) => {
+        res.send('This is ecommerce!');
+})
+
+app.listen(port, () => {
+        console.log('Server is running on http://localhost:' + port);
+})
